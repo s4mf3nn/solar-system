97dd2590fc51d9cf94710beb96f69a153efcb3e4
Samfenn <samir.fennikh@gmail.com>
2023-03-01 18:50:01 +0100

    Add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { Poppins } from '@next/font/google';
 import { useStore } from '@/store';
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'] });
+import { Analytics } from '@vercel/analytics/react';
 
 interface CustomPageProps { }
 function MyApp({ Component, pageProps }: AppProps<CustomPageProps>) {
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps<CustomPageProps>) {
     <>
       <div className={poppins.className}>
         <Component {...pageProps} />
+        <Analytics />
       </div>
       <style jsx global>{`
         body { background: ${backgroundColor}; }
